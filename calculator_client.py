40763f092bed586b68cd112bc7ae245cdad6d013
SarthakSinghal15 <sarthaksinghal15@gmail.com>
2018-09-09 22:56:42 -0700

    new input values

--- calculator_client.py
+++ calculator_client.py
@@ -9,8 +9,8 @@ import calculator_pb2_grpc
 def start():
     channel = grpc.insecure_channel('localhost:50051')
     stub = calculator_pb2_grpc.AdditionStub(channel)
-    integer1=12
-    integer2=122
+    integer1=34
+    integer2=45
     response = stub.add(calculator_pb2.AddRequest(number1=integer1,number2=integer2))
     print("The addition of "+str(integer1)+" and "+str(integer2)+" is " + str(response.message))
 
